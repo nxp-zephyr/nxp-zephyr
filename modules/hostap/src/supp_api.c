93f06bf4ebdc5dfcab51e1dbbe447c6295076061
Fengming Ye <frank.ye@nxp.com>
2024-06-20 14:47:02 +0900

    [nxp noup] hostap: add AP network security type DPP
    Add DPP security type for AP DPP support.
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- modules/hostap/src/supp_api.c
+++ modules/hostap/src/supp_api.c
@@ -1486,6 +1486,17 @@ int hapd_config_network(struct hostapd_iface *iface, struct wifi_connect_req_par
 			if (!hostapd_cli_cmd_v("set sae_pwe 2"))
 				goto out;
 			iface->bss[0]->conf->sae_pwe = 2;
+		} else if (params->security == WIFI_SECURITY_TYPE_DPP) {
+			if (!hostapd_cli_cmd_v("set wpa 2"))
+				goto out;
+			if (!hostapd_cli_cmd_v("set wpa_key_mgmt DPP"))
+				goto out;
+			if (!hostapd_cli_cmd_v("set wpa_passphrase %s", params->psk))
+				goto out;
+			if (!hostapd_cli_cmd_v("set wpa_pairwise CCMP"))
+				goto out;
+			if (!hostapd_cli_cmd_v("set rsn_pairwise CCMP"))
+				goto out;
 		}
 	} else {
 		if (!hostapd_cli_cmd_v("set wpa 0"))
