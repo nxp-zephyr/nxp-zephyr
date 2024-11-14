6324ca726f108602a7efb53e44ee4c160850b9ca
David Leach <david.leach@nxp.com>
2024-11-14 14:40:05 -0600

    Revert "[nxp noup] samples: net: wifi: add sap crypto enterprise for zephyr"
    This reverts commit a8b59c86c3128a3f79b9ca119423645d2cba3ba3.
    
    Signed-off-by: David Leach <david.leach@nxp.com>

--- modules/hostap/src/supp_api.h
+++ modules/hostap/src/supp_api.h
@@ -19,8 +19,7 @@
 #define MAC_STR_LEN 18 /* for ':' or '-' separated MAC address string */
 #define CHAN_NUM_LEN 6 /* for space-separated channel numbers string */
 
-#if defined CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ENTERPRISE || \
-	defined CONFIG_WIFI_NM_HOSTAPD_CRYPTO_ENTERPRISE
+#ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ENTERPRISE
 struct wpa_supp_enterprise_file {
 	uint8_t *ca_cert;
 	uint32_t ca_cert_len;
@@ -34,12 +33,6 @@ struct wpa_supp_enterprise_file {
 	uint32_t client_cert2_len;
 	uint8_t *client_key2;
 	uint32_t client_key2_len;
-	uint8_t *server_cert;
-	uint32_t server_cert_len;
-	uint8_t *server_key;
-	uint32_t server_key_len;
-	uint8_t *dh_param;
-	uint32_t dh_param_len;
 };
 #endif
 /**
@@ -229,8 +222,7 @@ int supplicant_pmksa_flush(const struct device *dev);
  */
 struct wifi_connect_req_params *supplicant_get_wifi_conn_params(void);
 
-#if defined CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ENTERPRISE || \
-	defined CONFIG_WIFI_NM_HOSTAPD_CRYPTO_ENTERPRISE
+#ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ENTERPRISE
 /** Set Wi-Fi enterprise mode CA/client Cert and key file
  *
  * @param file Pointer to the CA/client Cert and key file.
