40d6360026091743324feb2c95f8435ceb84cd2f
waqar tahir <waqar.tahir@nxp.com>
2026-03-17 10:06:15 +0100

    [nxp noup] soc: rdrw61x: clock init with tfm.
    - soc file is updated to allow clock init for peripherals
     which can be defined via dts config while
     TRUSTED_EXECUTION_NONSECURE is enabled.
    
    Signed-off-by: waqar tahir <waqar.tahir@nxp.com>

--- soc/nxp/rw/soc.c
+++ soc/nxp/rw/soc.c
@@ -245,6 +245,8 @@ __weak __ramfunc void clock_init(void)
 	/* Call function set_flexspi_clock() to set flexspi clock source to aux0_pll_clk in XIP. */
 	set_flexspi_clock(FLEXSPI, 2U, 2U);
 
+#endif /* ! CONFIG_TRUSTED_EXECUTION_NONSECURE */
+
 #if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(os_timer), nxp_os_timer, okay)
 	CLOCK_AttachClk(kLPOSC_to_OSTIMER_CLK);
 #endif
@@ -258,6 +260,7 @@ __weak __ramfunc void clock_init(void)
 	CLOCK_AttachClk(kNONE_to_WDT0_CLK);
 #endif
 
+#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 #if defined(CONFIG_ADC_MCUX_GAU) || defined(CONFIG_DAC_MCUX_GAU) || \
 	defined(CONFIG_COMPARATOR_NXP_ACOMP)
 	/* Set 64M GAU clock from T3 PLL 256M and reset */
@@ -267,6 +270,7 @@ __weak __ramfunc void clock_init(void)
 	RESET_PeripheralReset(kGAU_RST_SHIFT_RSTn);
 	GAU_BG->CTRL &= ~BG_CTRL_PD_MASK;
 #endif /* GAU */
+#endif /* ! CONFIG_TRUSTED_EXECUTION_NONSECURE */
 
 /* Any flexcomm can be USART */
 #if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm0), nxp_lpc_usart, okay)) && CONFIG_SERIAL
@@ -412,7 +416,6 @@ __weak __ramfunc void clock_init(void)
 	RESET_PeripheralReset(kENET_IPG_S_RST_SHIFT_RSTn);
 #endif
 
-#endif /* ! CONFIG_TRUSTED_EXECUTION_NONSECURE */
 }
 
 extern void nxp_rw6xx_power_init(void);
@@ -446,9 +449,12 @@ void soc_early_init_hook(void)
 
 	POWER_EnableResetSource(RESET_CAUSES);
 
+#endif /* ! CONFIG_TRUSTED_EXECUTION_NONSECURE */
+
 	/* Initialize clock */
 	clock_init();
 
+#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 #if defined(CONFIG_ADC_MCUX_GAU) || defined(CONFIG_DAC_MCUX_GAU)
 	POWER_PowerOnGau();
 #endif
